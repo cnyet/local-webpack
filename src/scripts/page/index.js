af56e9ebb3187774b63394f036be5729447caced
yate <yateup@163.com>
2017-09-30 14:19:16 +0800

    ad files

--- src/scripts/page/index.js
+++ src/scripts/page/index.js
@@ -2,4 +2,4 @@ import "../../styles/common/common.less";
 import "../../styles/common/variable.less";
 import "../../styles/page/index.css";
 
-document.getElementsByClassName("wrap").innerHTML = '<p>这是由js生成的一句话。</p>';
+document.getElementsByClassName("wrap")[0].innerHTML = '<p>这是由js生成的一句话。</p>';
