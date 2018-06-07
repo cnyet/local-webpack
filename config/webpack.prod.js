8ff96bbd22a1c74ef9cecfa8a71471297147a2cd
cnyet <yateup@gmail.com>
2018-06-07 10:54:01 +0800

    update package.json

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -16,6 +16,10 @@ function htmlPageArr(dir) {
         inject: 'body', //js插入的位置，true/'head'/'body'/false
         hash: true,                         //为静态资源生成hash值
         chunks: [value, "commons"], //需要引入的chunk，不配置就会引入所有页面的资源
+        //配置插入页面的meta属性
+        meta: {
+          viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+        },
         minify: { //压缩HTML文件
           removeComments: true, //移除HTML中的注释
           collapseWhitespace: true //删除空白符与换行符
